c5234f8b0ad26d0ea6d8d7a0af8568ca8773c0ed
vm4music <vm4music@gmail.com>
2022-12-06 02:41:58 +0800

    Vikas-portfolio-design

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+
+app.set('view engine', 'ejs')
+
+//Router Imports
+// const userRoute = require('./routers/userRoute')
+
+app.get('/', (req, res) => {
+    console.log('Here');
+    res.render('index')
+})
+
+app.use('/assets', express.static(__dirname + '/assets'));
+// app.use('/users', userRoute)
+
+app.listen(3000)
